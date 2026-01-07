76c4e535eb62b0dea8bca75ce661399c2e06d080
Swayam Gupta <swayamgupta193@gmail.com>
2026-01-07 21:30:45 +0530

    slight error in schema fixed it

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -55,7 +55,7 @@ const userSchema = new mongoose.Schema(
 },{timestamps:true})
 
 userSchema.pre("save",async function(next){
-    if (!this.ismodified("password")) return next()
+    if (!this.isModified("password")) return ;
 
     this.password= await bcrypt.hash(this.password,10)
     next()
