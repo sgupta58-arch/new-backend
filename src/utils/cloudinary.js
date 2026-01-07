09e544ea0918979eaa72ca8da92d443e623aeae7
Swayam Gupta <swayamgupta193@gmail.com>
2026-01-07 21:29:24 +0530

    fixed bugs

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,7 +2,7 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs"
 
     cloudinary.config({ 
-        cloud_name: process.env.CLOUD_NAME, 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
         api_key: process.env.CLOUDINARY_API_KEY, 
         api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
     });
@@ -14,6 +14,8 @@ import fs from "fs"
             const response= await cloudinary.uploader.upload(localFilepath,{resource_type: "auto"})
             console.log("File is uploaded on cloudinary",
             response.url)
+
+            fs.unlinkSync(localFilepath)
             return response
         
         } catch (error) {
