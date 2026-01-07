e9bd66897a374bed2fef4bc8d70174281ac4b260
Swayam Gupta <swayamgupta193@gmail.com>
2026-01-07 21:32:03 +0530

    read

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -6,10 +6,10 @@ const storage = multer.diskStorage({
   },
   filename: function (req, file, cb) {
     
-    cb(null, file.fieldname)
+    cb(null, `${Date.now()}-${file.originalname}`);
   }
 })
 
 export const upload = multer({
-     storage: storage
-     })
+    storage: storage
+    })
