c423613b54a2b3f153adbad31132a6972c546bb7
Swayam Gupta <swayamgupta193@gmail.com>
2026-01-26 23:19:37 +0530

    minor correction

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { registerUser,loginUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 
-const router = Router()
-router.route("/register").post(
+const userRouter = Router()
+userRouter.route("/register").post(
     upload.fields([
         {
             name:"avatar",
@@ -17,6 +17,6 @@ router.route("/register").post(
     
     registerUser)
 
-router.route("/login").post(loginUser)  
+userRouter.route("/login").post(loginUser)  
 
-export default router
+export default userRouter
